8ca938b2b0d4bff81de68daa225902f1d1ef41f0
TinOo512 <etienne@kontestapp.com>
2015-11-05 14:31:20 +0100

    bind `this` to ensure we don't loose context
    fix `Cannot read property 'equals' of undefined`

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 export default function addAssertions (tape, assertions) {
-    return (testName, testBody) => tape(testName, (t) => {
-        Object.assign(t, assertions);
-        testBody(t);
+    return (testName, testBody) => tape(testName, function (t) {
+        for(let prop in assertions) {
+            t[prop] = assertions[prop].bind(t);
+        }
+        testBody.call(this, t);
     });
 }
